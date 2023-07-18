f5fc579662a1676c7c2a90b4191fab4ebe8d69c6
085OrlovAE <sanyok123128@gmail.com>
2023-07-18 15:21:03 +0900

    Change toggle navbar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,40 +1,62 @@
-
+//navbar-toggle - true это открытое меню, а false - закрытое
 $(document).ready(function () {
     if (window.innerWidth < 992.99) {
-        localStorage.setItem("navbar-toggle", "true");
+        localStorage.setItem("navbar-toggle", "false");
     }
     let stateNavbar = localStorage.getItem('navbar-toggle');
+    let navbarToggle = $("#navbar-toggle");
+    let contentContainer = $("#content-main");
+
     if (stateNavbar === "true") {
-        if (!$("#navbar-toggle").hasClass("navbarMobile")) {
-            $("#navbar-toggle").addClass("navbarMobile");
-            $("#content-toggle").addClass("contentMobile");
+        if (navbarToggle.hasClass("navbarMobile")) {
+            navbarToggle.removeClass("navbarMobile");
+            contentContainer.removeClass("closeNavbar");
         }
     } else {
-        if ($("#navbar-toggle").hasClass("navbarMobile")) {
-            $("#navbar-toggle").removeClass("navbarMobile");
-            $("#content-toggle").removeClass("contentMobile");
+        if (!navbarToggle.hasClass("navbarMobile")) {
+            navbarToggle.addClass("navbarMobile");
+            contentContainer.addClass("closeNavbar");
         }
     }
 });
 
 $(window).resize(() => {
-    if (window.innerWidth < 992.99) {
-        localStorage.setItem("navbar-toggle", "true");
-        if (!$("#navbar-toggle").hasClass("navbarMobile")) {
-            $("#navbar-toggle").addClass("navbarMobile");
-            $("#content-toggle").addClass("contentMobile");
+    let navbarToggle = $("#navbar-toggle");
+    let contentContainer = $("#content-main");
+    if (window.localStorage.getItem("navbar-toggle") === "true"){
+        if (window.innerWidth < 992.99) {
+            if (!navbarToggle.hasClass("navbarMobile") && !contentContainer.hasClass("closeNavbar")) {
+                navbarToggle.addClass("navbarMobile");
+                contentContainer.addClass("closeNavbar");
+            }
+            localStorage.setItem("navbar-toggle", "false");
+        } else {
+            if (navbarToggle.hasClass("navbarMobile") && contentContainer.hasClass("closeNavbar")) {
+                navbarToggle.removeClass("navbarMobile");
+                contentContainer.removeClass("closeNavbar");
+            }
+            localStorage.setItem("navbar-toggle", "true");
         }
     } else {
-        if (window.localStorage.getItem("navbar-toggle") === "false")
-        if ($("#navbar-toggle").hasClass("navbarMobile")) {
-            $("#navbar-toggle").removeClass("navbarMobile");
-            $("#content-toggle").removeClass("contentMobile");
+
+        if (!navbarToggle.hasClass("navbarMobile")) {
+            navbarToggle.addClass("navbarMobile");
         }
+
+        localStorage.setItem("navbar-toggle", "false");
     }
 })
 
 function toggleNavbarScript() {
-    $("#navbar-toggle").toggleClass("navbarMobile");
-    $("#content-toggle").toggleClass("contentMobile");
-    localStorage.setItem("navbar-toggle", $("#navbar-toggle").hasClass("navbarMobile"));
+    let navbarToggle = $("#navbar-toggle");
+    let contentContainer = $("#content-main");
+
+    if (window.innerWidth < 992.99) {
+        navbarToggle.toggleClass("navbarMobile");
+        if (!contentContainer.hasClass("closeNavbar")) { contentContainer.addClass("closeNavbar"); }
+    } else {
+        navbarToggle.toggleClass("navbarMobile");
+        contentContainer.toggleClass("closeNavbar");
+        localStorage.setItem("navbar-toggle", !navbarToggle.hasClass("navbarMobile"));
+    }
 }
