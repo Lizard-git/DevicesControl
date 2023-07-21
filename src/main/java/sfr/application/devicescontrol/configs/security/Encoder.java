bd49386e2e3835dddc8f5ca25e6d5436cb5ccb89
085OrlovAE <sanyok123128@gmail.com>
2023-07-21 14:29:13 +0900

    Add encoder

---
+++ src/main/java/sfr/application/devicescontrol/configs/security/Encoder.java
@@ -0,0 +1,18 @@
+package sfr.application.devicescontrol.configs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class Encoder {
+    /**
+     * Bean для шифрования пароля с помощью BCrypt
+     * @return зашифрованный пароль
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
