1b0aef02df1e1088d4121c18c5da6e7255576bc7
085OrlovAE <sanyok123128@gmail.com>
2023-08-28 16:49:21 +0900

    rewritten the history service

--- src/main/java/sfr/application/devicescontrol/services/HistoryService.java
+++ src/main/java/sfr/application/devicescontrol/services/HistoryService.java
@@ -4,138 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import sfr.application.devicescontrol.entities.telbook.devices_control.HistoryEntity;
 import sfr.application.devicescontrol.entities.telbook.devices_control.UserEntity;
 import sfr.application.devicescontrol.enums.TypeMessagesHistory;
 import sfr.application.devicescontrol.repositories.telbook.device_control.HistoryRepository;
+import sfr.application.devicescontrol.utils.IpAddressVerificationUtils;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class HistoryService {
-    private static final String IPADDRESS_PATTERN =
-            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
-
     private final HistoryRepository historyRepository;
-
-    /**
-     * Добавляет новую запись истории
-     * @param change - изменения
-     * @param ipAddress - ip адресс с которого производятся изменения
-     * @param type - тип сообщения Error, Info, Warning
-     * @param user - пользователь, который совершает действие
-     * @param dopInfo - Дополнительная информация для записи
-     * @throws UnknownHostException - Ошибка получения ip адреса
-     */
-    public void newHistory(String change, String ipAddress, TypeMessagesHistory type, UserEntity user, String dopInfo) throws UnknownHostException {
-        ipAddress = ipAddressValidator(ipAddress);
-        historyRepository.save(HistoryEntity.builder()
-                        .textChange(change)
-                        .dataHistory(new Date())
-                        .user(user)
-                        .ipAddress(ipAddress)
-                        .type(type)
-                        .dopInfo(dopInfo)
-                        .build());
-    }
-    /**
-     * Добавляет новую запись истории
-     * @param change - изменения
-     * @param ipAddress - ip адресс с которого производятся изменения
-     * @param type - тип сообщения Error, Info, Warning
-     * @param user - пользователь, который совершает действие
-     * @throws UnknownHostException - Ошибка получения ip адреса
-     */
-    public void newHistory(String change, String ipAddress, TypeMessagesHistory type, UserEntity user) throws UnknownHostException {
-        ipAddress = ipAddressValidator(ipAddress);
-        historyRepository.save(HistoryEntity.builder()
-                .textChange(change)
-                .dataHistory(new Date())
-                .user(user)
-                .ipAddress(ipAddress)
-                .type(type)
-                .build());
-    }
-    /**
-     * Добавляет новую запись истории типа Error
-     * @param change - изменения
-     * @param ipAddress - ip адресс с которого производятся изменения
-     * @param user - пользователь, который совершает действие
-     * @throws UnknownHostException - Ошибка получения ip адреса
-     */
-    public void newHistoryError(String change, String ipAddress, UserEntity user) throws UnknownHostException {
-        ipAddress = ipAddressValidator(ipAddress);
-        historyRepository.save(HistoryEntity.builder()
-                .textChange(change)
-                .dataHistory(new Date())
-                .user(user)
-                .ipAddress(ipAddress)
-                .type(TypeMessagesHistory.Error)
-                .build());
-    }
-
-    /**
-     * Добавляет новую запись истории типа Info
-     * @param change - изменения
-     * @param ipAddress - ip адресс с которого производятся изменения
-     * @param user - пользователь, который совершает действие
-     * @throws UnknownHostException - Ошибка получения ip адреса
-     */
-    public void newHistoryInfo(String change, String ipAddress, UserEntity user) throws UnknownHostException {
-        ipAddress = ipAddressValidator(ipAddress);
-        historyRepository.save(HistoryEntity.builder()
-                .textChange(change)
-                .dataHistory(new Date())
-                .user(user)
-                .ipAddress(ipAddress)
-                .type(TypeMessagesHistory.Info)
-                .build());
-    }
-
-    /**
-     * Добавляет новую запись истории типа Warning
-     * @param change - изменения
-     * @param ipAddress - ip адресс с которого производятся изменения
-     * @param user - пользователь, который совершает действие
-     * @throws UnknownHostException - Ошибка получения ip адреса
-     */
-    public void newHistoryWarning(String change, String ipAddress, UserEntity user) throws UnknownHostException {
-        ipAddress = ipAddressValidator(ipAddress);
-        historyRepository.save(HistoryEntity.builder()
-                .textChange(change)
-                .dataHistory(new Date())
-                .user(user)
-                .ipAddress(ipAddress)
-                .type(TypeMessagesHistory.Warning)
-                .build());
-    }
-
-    /**
-     * Валидация ip адреса
-     * @param ipAddress - страка ip адреса
-     * @return ip address
-     * @throws UnknownHostException - ошибка получания ip адресса
-     */
-    public String ipAddressValidator(String ipAddress) throws UnknownHostException {
-        Pattern pattern = Pattern.compile(IPADDRESS_PATTERN);
-        Matcher matcher = pattern.matcher(ipAddress);
-        if (matcher.matches()) {
-            return ipAddress;
-        }
-        InetAddress address1 = InetAddress.getLocalHost();
-        return address1.getHostAddress();
-    }
+    private final UserService userService;
 
     /**
      * Получает всю историяю из базы данных
@@ -152,5 +38,97 @@ public class HistoryService {
      */
     public Page<HistoryEntity> getAllHistory(Pageable pageable) {
         return historyRepository.findAll(pageable);
+    }
+
+    /**
+     * Метод для получения текущего пользователя сессии
+     * @return UserEntity
+     */
+    private UserEntity getCurrantUser() {
+        return userService.getUserByLoginNotDeleted(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    /**
+     * Добавляет новую запись истории
+     * @param change - изменения
+     * @param ipAddress - ip адресс с которого производятся изменения
+     * @param type - тип сообщения Error, Info, Warning
+     * @param dopInfo - Дополнительная информация для записи
+     * @throws UnknownHostException - Ошибка получения ip адреса
+     */
+    public void newHistory(String change, String ipAddress, TypeMessagesHistory type, String dopInfo) throws UnknownHostException {
+        historyRepository.save(HistoryEntity.builder()
+                        .textChange(change)
+                        .dataHistory(new Date())
+                        .user(getCurrantUser())
+                        .ipAddress(IpAddressVerificationUtils.ipAddressValidator(ipAddress))
+                        .type(type)
+                        .dopInfo(dopInfo)
+                        .build());
+    }
+
+    /**
+     * Добавляет новую запись истории
+     * @param change - изменения
+     * @param ipAddress - ip адресс с которого производятся изменения
+     * @param type - тип сообщения Error, Info, Warning
+     * @throws UnknownHostException - Ошибка получения ip адреса
+     */
+    public void newHistory(String change, String ipAddress, TypeMessagesHistory type) throws UnknownHostException {
+        historyRepository.save(HistoryEntity.builder()
+                .textChange(change)
+                .dataHistory(new Date())
+                .user(getCurrantUser())
+                .ipAddress(IpAddressVerificationUtils.ipAddressValidator(ipAddress))
+                .type(type)
+                .build());
+    }
+
+    /**
+     * Добавляет новую запись истории типа Error
+     * @param change - изменения
+     * @param ipAddress - ip адресс с которого производятся изменения
+     * @throws UnknownHostException - Ошибка получения ip адреса
+     */
+    public void newHistoryError(String change, String ipAddress) throws UnknownHostException {
+        historyRepository.save(HistoryEntity.builder()
+                .textChange(change)
+                .dataHistory(new Date())
+                .user(getCurrantUser())
+                .ipAddress(IpAddressVerificationUtils.ipAddressValidator(ipAddress))
+                .type(TypeMessagesHistory.Error)
+                .build());
+    }
+
+    /**
+     * Добавляет новую запись истории типа Info
+     * @param change - изменения
+     * @param ipAddress - ip адресс с которого производятся изменения
+     * @throws UnknownHostException - Ошибка получения ip адреса
+     */
+    public void newHistoryInfo(String change, String ipAddress) throws UnknownHostException {
+        historyRepository.save(HistoryEntity.builder()
+                .textChange(change)
+                .dataHistory(new Date())
+                .user(getCurrantUser())
+                .ipAddress(IpAddressVerificationUtils.ipAddressValidator(ipAddress))
+                .type(TypeMessagesHistory.Info)
+                .build());
+    }
+
+    /**
+     * Добавляет новую запись истории типа Warning
+     * @param change - изменения
+     * @param ipAddress - ip адресс с которого производятся изменения
+     * @throws UnknownHostException - Ошибка получения ip адреса
+     */
+    public void newHistoryWarning(String change, String ipAddress) throws UnknownHostException {
+        historyRepository.save(HistoryEntity.builder()
+                .textChange(change)
+                .dataHistory(new Date())
+                .user(getCurrantUser())
+                .ipAddress(IpAddressVerificationUtils.ipAddressValidator(ipAddress))
+                .type(TypeMessagesHistory.Warning)
+                .build());
     }
 }
